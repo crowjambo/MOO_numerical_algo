2488589d89b6a974e7a1df7d147072b8d063830f
Evaldas <evaldascrow@gmail.com>
2019-10-02 10:59:18 +0300

    crossing method init

---
+++ TASK5_Crossing_method/CrossingMethod.cs
@@ -0,0 +1,15 @@
+using System;
+
+
+public class CrossingMethod{
+    public double? ExecuteNewCrossing(
+        Func<double,double> function,
+        double error
+    ){
+        //variables
+        Func<double,double> f = function;
+
+        int n = 0; //number of divisions
+        int i = 0; //iterations
+    }
+}
---
+++ TASK5_Crossing_method/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace TASK5_Crossing_method
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            CrossingMethod equation = new CrossingMethod();
+            Func<double,double> f = x => Math.Pow(x,3)-x-0.231;
+            //derivative
+            Func<double,double> g = x => 3*Math.Pow(x,2)-1;
+
+            double? root = equation.ExecuteNewCrossing();
+
+            Console.WriteLine($"Root is {root}");
+        }
+    }
+}
