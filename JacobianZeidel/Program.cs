67ba31f13998f5659fc8a644240ad3f79a49baed
Evaldas <evaldascrow@gmail.com>
2019-11-21 15:37:17 +0200

    zeidel and jacobian fully working with examples

--- JacobianZeidel/Program.cs
+++ JacobianZeidel/Program.cs
@@ -9,86 +9,6 @@ namespace IterativeSolutions
         //compare jacobian vs zeidel speed etc.
 
 
-        // jacobian solution
-        public static double[] JacobianSolution(double[][] inputMatrix, double precision){
-            // double x1 = 0;
-            List<double> x1 = new List<double>();
-            List<double> x2 = new List<double>();
-            List<double> x3 = new List<double>();
-            x1.Add(0);
-            x2.Add(0);
-            x3.Add(0);
-            double calcResult;
-            double calcResult1;
-            double calcResult2;
-
-            // double x2 =0;
-            // double x3 = 0;
-            // double tempx1 = 0;
-            // double tempx2 = 0;
-            // double tempx3 = 0;
-            int i = 0;
-            bool found = false;
-            //some loop while, do epsilon check here
-            System.Console.WriteLine("JACOBIAN");
-            while(!found){
-
-                //problem with writing the proper equation due to - + signs!!! fucks shit up
-                //if [0][1] = - sign in front, it needs to be multiplied by -1 to maintain negative and not turn into +
-                //same for [2][1] or [1][1](if it were a negative number, needs to be multiplied by -1)
-                if(inputMatrix[0][1] < 0){
-                    inputMatrix[0][1] *= -1;
-                }
-                if(inputMatrix[1][1] < 0){
-                    inputMatrix[1][1] *= -1;
-                }
-                if(inputMatrix[2][1] < 0){
-                    inputMatrix[2][1] *= -1;
-                }
-
-                calcResult = inputMatrix[0][0] * x2[i] - (inputMatrix[0][1] * x3[i]) + inputMatrix[0][2];
-                x1.Add(calcResult);
-                calcResult1 = inputMatrix[1][0] * x1[i] - inputMatrix[1][1] *x3[i] + inputMatrix[1][2];
-                x2.Add(calcResult1);
-                calcResult2 = inputMatrix[2][0] * x1[i] - (inputMatrix[2][1] * x2[i]) + inputMatrix[2][2];
-                x3.Add(calcResult2); 
-                //calc with current values
-                // tempx1 = x1;
-                // tempx2 = x2; 
-                // tempx3 = x3; 
-                // //iterat 1 x1=1.1 x2= 2.25 x3= 1.4
-                // //iterat2 x1=0.905 x2=1.93 x3=0.955
-                // x1 = (0.1 * tempx2) - (-0.3 * tempx3) + 1.1;
-                // x2 = (-0.1 * tempx1) - (-0.3 * tempx3) + 2.25;
-                // x3 = (-0.2 * tempx1) - (2.25 * tempx2) + 1.4;
-                // x1 = (inputMatrix[0][0] * tempx2) - (inputMatrix[0][1] * tempx3) + inputMatrix[0][2];
-                // x2 = (inputMatrix[1][0] * tempx1) - (inputMatrix[1][1] * tempx3) + inputMatrix[1][2];
-                // x3 = (inputMatrix[2][0] * tempx1) - (inputMatrix[2][1] * tempx2) + inputMatrix[2][2];
-
-                // System.Console.WriteLine($"Iteration {i} x1 = {x1} x2 = {x2} x3 = {x3}");
-
-                i++;
-                System.Console.WriteLine($"Iteration {i} x1 = {x1[i]} x2 = {x2[i]} x3 = {x3[i]}");
-                //final precision check
-                if(Math.Abs(x1[i] - x1[i-1]) < precision && Math.Abs(x2[i] - x2[i-1]) < precision && Math.Abs(x3[i] - x3[i-1]) < precision){
-                    found = true;
-                }
-                // if(Math.Abs(tempx1-x1) < precision && Math.Abs(tempx2-x2) < precision && Math.Abs(tempx3-x3) < precision){
-                //     found = true;
-                // }
-            }
-
-
-
-            double[] results = new double[3];
-            results[0] = x1[i];
-            results[1] = x2[i];
-            results[2] = x3[i];
-            //return all x values in array
-            return results;
-
-        }
-
         //zeidels solution
         public static double[] ZeidelSolution(double[][] inputMatrix, double precision){
             double x1 = 0;
@@ -97,28 +17,75 @@ namespace IterativeSolutions
             double tempx1 = 0;
             double tempx2 = 0;
             double tempx3 = 0;
-            int i = 1;
+
+            //previous test
+            double prev1 = 0;
+            double prev2 = 0;
+            double prev3 = 0;
+
+            int i = 0;
             bool found = false;
-            //some loop while, do epsilon check here
             System.Console.WriteLine("ZEIDEL");
             while(!found){
-                //save previous values
-                tempx1 = x1; tempx2 = x2; tempx3 = x3;
-                //calc with current values
-                x1 = inputMatrix[0][0] * x2 - inputMatrix[0][1] * x3 + inputMatrix[0][2];
-                x2 = inputMatrix[1][0] * x1 - inputMatrix[1][1] * x3 + inputMatrix[1][2];
-                x3 = inputMatrix[2][0] * x1 - inputMatrix[2][1] * x2 + inputMatrix[2][2];
 
-                System.Console.WriteLine($"Iteration {i} x1 = {x1} x2 = {x2} x3 = {x3}");
+                prev1 = x1;
+                prev2 = x2;
+                prev3 = x3;
+
+                //difference is that, we use our calc values in next calculation without waiting for next iteration
+                x1 = (inputMatrix[0][0] * tempx2) + (inputMatrix[0][1] * tempx3) + inputMatrix[0][2];
+                tempx1 = x1;
+                x2 = (inputMatrix[1][0] * tempx1) + (inputMatrix[1][1] * tempx3) + inputMatrix[1][2];
+                tempx2 = x2;
+                x3 = (inputMatrix[2][0] * tempx1) + (inputMatrix[2][1] * tempx2) + inputMatrix[2][2];
+                tempx3 = x3;
 
                 i++;
+                System.Console.WriteLine($"Iteration {i} x1 = {x1} x2 = {x2} x3 = {x3}");
+
+                //final precision check
+                if(Math.Abs(x1 - prev1) < precision && Math.Abs(x2 - prev2) < precision && Math.Abs(x3 - prev3) < precision){
+                    found = true;
+                }
+            }
+
+            double[] results = new double[3];
+            results[0] = x1;
+            results[1] = x2;
+            results[2] = x3;
+            //return all x values in array
+            return results;
+        }
+
+        //jacobian solution
+        public static double[] JacobianSolution(double[][] inputMatrix, double precision){
+            double x1 = 0;
+            double x2 =0;
+            double x3 = 0;
+            double tempx1 = 0;
+            double tempx2 = 0;
+            double tempx3 = 0;
+            int i = 0;
+            bool found = false;
+            System.Console.WriteLine("JACOBIAN");
+            while(!found){
+
+                //calc with current values
+                tempx1 = x1;
+                tempx2 = x2; 
+                tempx3 = x3; 
+                x1 = (inputMatrix[0][0] * tempx2) + (inputMatrix[0][1] * tempx3) + inputMatrix[0][2];
+                x2 = (inputMatrix[1][0] * tempx1) + (inputMatrix[1][1] * tempx3) + inputMatrix[1][2];
+                x3 = (inputMatrix[2][0] * tempx1) + (inputMatrix[2][1] * tempx2) + inputMatrix[2][2];
+
+                i++;
+                System.Console.WriteLine($"Iteration {i} x1 = {x1} x2 = {x2} x3 = {x3}");
+
                 //final precision check
                 if(Math.Abs(tempx1-x1) < precision && Math.Abs(tempx2-x2) < precision && Math.Abs(tempx3-x3) < precision){
                     found = true;
                 }
             }
-
-
 
             double[] results = new double[3];
             results[0] = x1;
@@ -129,43 +96,112 @@ namespace IterativeSolutions
 
         }
 
+        public static double[][] MatrixConvert(double[][] inputMatrix){
+            double[][] newMatrix = new double[3][]{
+                new double[3],
+                new double[3],
+                new double[3]
+            };
+        
+            for (int i = 0; i < 3; i++)
+            {
+                //convert 3x4 straight input matrix into 3x3 where its divided and prepared for operations
+                for (int j = 0; j < 3; j++)
+                {
+                    
+                    //first row, skip first element x1
+                    if(i==0){
+                        newMatrix[i][j] = (inputMatrix[i][j+1] / inputMatrix[i][i]) *(-1);
+                    }
+                    //in 2nd row, skip second element (x2)
+                    else if(i==1){
+                        if(j==1){
+                            newMatrix[i][j] = (inputMatrix[i][j+1] / inputMatrix[i][i]) *(-1);
+                        }
+                        else if(j==2){
+                            newMatrix[i][j] = (inputMatrix[i][j+1] / inputMatrix[i][i]) *(-1);
+                        }
+                        else{
+                            newMatrix[i][j] = (inputMatrix[i][j] / inputMatrix[i][i]) *(-1);
+                        }
+                        
+                    }
+                    //3rd row, skip x3 element
+                    else{
+                        if(j==2){
+                            newMatrix[i][j] = (inputMatrix[i][j+1] / inputMatrix[i][i]) *(-1);
+                        }  
+                        else{
+                            newMatrix[i][j] = (inputMatrix[i][j] / inputMatrix[i][i]) *(-1);
+                        }
+                        
+                    }
+                    //reverse -1 multipication on last number ( normal numbers ) as they dont get moved
+                    if(j+1 == 3){
+                        newMatrix[i][j] *= -1;
+                    }
+                }
+            }
 
+            return newMatrix;
 
+        }
 
         static void Main(string[] args)
         {
             
+            // ---------------------------------------------------------------- VARS
+
             //already pretransformed into what I need ( can later exchange with function that transforms )
-            double[][] inputMatrix = new double[][] {
-                new double[]{0.1,-0.3,1.1},
-                new double[]{-0.1,0.15,2.25},
-                new double[]{-0.2,-0.1,1.4}
+            // double[][] inputMatrix = new double[][] {
+            //     new double[]{0.1,-0.3,1.1},
+            //     new double[]{-0.1,0.15,2.25},
+            //     new double[]{-0.2,-0.1,1.4}
+            // };
+
+            //non converted input
+            double[][] inputMatrix = new double[][]{
+                    new double[]{5,-1,2,12},
+                    new double[]{3,8,-2,-25},
+                    new double[]{1,1,4,6}
             };
+            // double[][] inputMatrix = new double[][]{
+            //         new double[]{10,-1,3,11},
+            //         new double[]{2,20,3,45},
+            //         new double[]{2,1,10,14}
+            // };
+            //convert
+            inputMatrix = MatrixConvert(inputMatrix); //converts into top commented example format
  
             double precision = 0.001;
-            //timer
+
+
+            // -------------------------------------------------------------- JACOBIAN
+            
+
             var watch = System.Diagnostics.Stopwatch.StartNew();
-            // jacobian
             var result = JacobianSolution(inputMatrix, precision);
             watch.Stop();
-            //loop for result Jacobian
+            
+            //output
             System.Console.WriteLine("Final answers Jacobian: \n ----------");
             foreach(var x in result){
                 System.Console.WriteLine(Math.Round(x));
             }
             System.Console.WriteLine("\n" + watch.Elapsed + "\n ---------");
 
-            // //zeidel
-            // watch = System.Diagnostics.Stopwatch.StartNew();
-            // result = ZeidelSolution(inputMatrix, precision);
-            // watch.Stop();
-            // System.Console.WriteLine("\n\n\n" + watch.Elapsed + "\n\n");
-            // //loop for result ZEIDEL
-            // System.Console.WriteLine("Final answers Zeidels: \n ----------");
-            // foreach(var x in result){
-            //     System.Console.WriteLine(x);
-            // }
+            // --------------------------------------------------------------- ZEIDEL
 
+            watch = System.Diagnostics.Stopwatch.StartNew();
+            result = ZeidelSolution(inputMatrix, precision);
+            watch.Stop();
+
+            //output
+            System.Console.WriteLine("Final answers Jacobian: \n----------");
+            foreach(var x in result){
+                System.Console.WriteLine(Math.Round(x));
+            }
+            System.Console.WriteLine("\n" + watch.Elapsed + "\n ---------");
 
             
         }
